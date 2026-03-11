1fa19e0c3767cfd68fa1b0bad63780594886967f
maxykoin <stayerim@gmail.com>
2026-03-11 19:43:22 -0300

    feat: compile sucks + aula 2

---
+++ src/aps1.cpp
@@ -0,0 +1,56 @@
+
+/**
+ * @file aps1.cpp
+ * @author Nina Cunha
+ * @date 2026-03-11
+ * @brief Valida operações matemáticas.
+ * * Exercício: Crie um programa em C++ que leia dois números e uma operação (+, -, * ou /).
+ * O programa deve calcular e mostrar o resultado da operação escolhida.
+ * Se a operação for inválida, mostre “Operação inválida”.
+ * Se for divisão por zero, mostre “Erro: divisão por zero”.
+ * * Disciplina: Design de Softwares para Sistemas Mecatrônicos
+ * Instituição: UniSENAI PR
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    float a; float b; 
+    int op;
+
+    cout << "Digite o primeiro número: ";
+    cin >> a;
+
+    cout << "Digite o segundo número: ";
+    cin >> b;
+
+    cout << "Digite a operação desejada: \n1- Soma\n2- Subtração\n3- Multiplicação\n4-Divisão\nEscolha: ";
+    cin >> op;
+
+    cout << "O resultado é: ";
+
+    switch(op){
+        case 1:
+            cout << a + b << endl;
+            break;
+        case 2:
+            cout << a - b << endl;
+            break;
+        case 3:
+            cout << a * b << endl;
+            break;
+        case 4:
+            if(b==0){
+                cout << "Erro: Divisão por zero" << endl;
+                break;
+            } else {
+                cout << a / b << endl;
+                break;
+            }
+        default:
+            cout << "Operação Inválida" << endl;
+            break;
+    }   
+    return 0;
+}
---
+++ src/aula02_at1.cpp
@@ -0,0 +1,33 @@
+
+/**
+ * @file aula02_atv01.cpp
+ * @author Nina Cunha
+ * @date 2026-03-11
+ * @brief Lê a velocidade de um motor e a classifica.
+ * * Exercício: Crie um programa que leia a velocidade de um motor e a classifique como:
+ * Baixa se < 500
+ * Média se entre 500 e 1500
+ * Alta se > 1500
+ * * Disciplina: Design de Softwares para Sistemas Mecatrônicos
+ * Instituição: UniSENAI PR
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    float rpm;
+
+    cout << "Digite o RPM: ";
+    cin >> rpm;
+
+    if (rpm < 500){
+        cout << "Seu rpm está baixo" << endl;  
+    } else if (rpm > 1500){
+        cout << "Seu rpm está alto" << endl;
+    } else {
+        cout << "Seu rpm está na média" << endl;
+    }
+
+    return 0;
+ }
